3010959c6bc31898a4ecc4cd80772ce74c0493e5
nabilamutiara <116213455+nabilamutiara@users.noreply.github.com>
2025-04-04 09:03:15 +0700

    writer page

--- client/src/dashboard/pages/CreateNews.jsx
+++ client/src/dashboard/pages/CreateNews.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { FaImages } from "react-icons/fa6";
+import JoditEditor from 'jodit-react';
+import Gallery from '../components/Gallery';
 
 const CreateNews = () => {
+    const [loader, setLoader] = useState(false);
+    const [show, setShow] = useState(false);
+    const [images, setImages] = useState([]);
     return (
-        <div>
-            create news
+        <div className='bg-white shadow-md rounded-md p-6 '>
+            <div className='flex justify-between items-center mb-6'>
+                <h2 className='text-2xl font-bold text-gray-700'>Add News</h2>
+                <Link className='px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-800 transition duration-300' to='/dashboard/news'>View All
+                </Link>
+            </div>
+            <form >
+                <div>
+                    <label htmlFor="title" className='block text-md font-medium text-gray-600 mb-2'>Title</label>
+                    <input type="text" placeholder='Enter News Title' name='title' id='title' className='w-full px-4 py-2 border rounded-md border-gray-300 focus:border-blue-500 outline-none transition h-10 ' required/>
+                </div>
+                <div>
+                    <label htmlFor="img" className='w-full h-[240px] flex flex-col items-center justify-center cursor-pointer border-2 border-dashed border-gray-500 rounded-lg text-gray-500 hover:border-blue-500 transition mt-4'>
+                        <FaImages className='text-4xl mb-2'/>
+                        <span className='font-medium'>Select Image</span>
+                    </label>
+                    <input type="file" className='hidden' id='img' required/>
+                </div>
+
+                <div>
+                    <div className='flex justify-between items-center mb-2 mt-4'> 
+
+                        <label htmlFor="description" className='block text-md font-medium text-gray-600'>Description</label>
+                        
+                        <div onClick={() => setShow(true)} className='text-blue-500 hover:text-blue-800 cursor-pointer'>
+                            <FaImages className='text-2xl'/>
+                        </div>
+                    </div>
+                    <JoditEditor className='w-full border border-gray-400 rounded-md'/>
+                    <div className='mt-4'>
+                        <button type='submit' disabled={loader} className='px-3 py-[6px] bg-blue-500 rounded-md text-white hover:bg-blue-800'>
+                            {loader ? 'Loading...' : 'Add News'}
+                        </button>
+                    </div>
+                </div>
+                
+            </form>
+            {show && <Gallery setShow={setShow} images={images}/>}
+                <input type="file" multiple id="images" className="hidden"/>
         </div>
     );
 };
--- client/src/dashboard/pages/Login.jsx
+++ client/src/dashboard/pages/Login.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login page</h1>
+        <div className='min-h-screen bg-slate-100 flex items-center justify-center'>
+            <div className='bg-white shadow-lg rounded-lg w-[400px]'>
+                <div className='p-8'>
+                    <div className='flex justify-center mb-8'>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
